f7f8f77b81e0fb5695cc71207ff2bef81213e3af
Marco Giammari <marcogiammari@gmail.com>
2023-04-26 17:01:13 +0200

    add(js) adaptive grid

--- js/main.js
+++ js/main.js
@@ -7,7 +7,16 @@ const playBtn = document.getElementById("play-btn");
 
 playBtn.addEventListener("click", function() {
     gridContainer.innerHTML = "";
-    const gridSize = document.querySelector("select").value
+    gridContainer.style.display = "grid"
+    const gridSize = document.querySelector("select").value;
+    if (gridSize==49) {
+        gridContainer.style.gridTemplateColumns = "repeat(7, 4rem)"
+    } else if (gridSize==81) {
+        gridContainer.style.gridTemplateColumns = "repeat(9, 3.5rem)"
+    }
+    else {
+        gridContainer.style.gridTemplateColumns = "repeat(10, 3rem)"
+    }
     startGame(gridSize)
 });
 
