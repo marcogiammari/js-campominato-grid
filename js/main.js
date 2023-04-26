83df87d9cf93aec1177483058e99cb52bc05356d
marcogiammari <marcogiammari@gmail.com>
2023-04-26 19:09:08 +0200

    add grid fixed dimensions + comments

--- js/main.js
+++ js/main.js
@@ -3,35 +3,35 @@
 const gridContainer = document.getElementById("grid");
 const playBtn = document.getElementById("play-btn");
 
-//
-
-playBtn.addEventListener("click", function() {
+// associo la funzione al bottone
+playBtn.addEventListener("click", function () {
+    // svuoto il contenuto della griglia
     gridContainer.innerHTML = "";
     gridContainer.style.display = "grid"
     const gridSize = document.querySelector("select").value;
-    if (gridSize==49) {
-        gridContainer.style.gridTemplateColumns = "repeat(7, 4rem)"
-    } else if (gridSize==81) {
-        gridContainer.style.gridTemplateColumns = "repeat(9, 3.5rem)"
+    // assegno a una variabile la difficoltà scelta dell'utente e la passo come argomento alla funzione startGame
+    if (gridSize == 49) {
+        gridContainer.style.gridTemplateColumns = "repeat(7, auto)"
+    } else if (gridSize == 81) {
+        gridContainer.style.gridTemplateColumns = "repeat(9, auto)"
     }
     else {
-        gridContainer.style.gridTemplateColumns = "repeat(10, 3rem)"
+        gridContainer.style.gridTemplateColumns = "repeat(10, auto)"
     }
     startGame(gridSize)
 });
 
+// definisco startGame
 function startGame(difficulty) {
-    
+
     for (let i = 1; i <= difficulty; i++) {
         let el = document.createElement("div");
         el.classList.add("square");
         el.innerText = i;
-        el.addEventListener("click", function() {
+        el.addEventListener("click", function () {
             this.classList.toggle("clicked");
             console.log(this.innerText)
         })
         gridContainer.appendChild(el);
     }
 }
-
-// ricordati di cambiare la width dei quadrati in base al numero totale
